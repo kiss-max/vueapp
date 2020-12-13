bfd911811f57ee2b8e65b0079ebefd193f0e8f3c
王嘉磊 <wangjl@ddbes.com>
2020-12-13 21:49:35 +0800

    1

--- README.md
+++ README.md
@@ -17,3 +17,20 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+v-model.number
+
+vue cli安装
+vue 指令创建项目
+npm运行vue项目指令
+工程目录每一个文件或者文件夹的功能作用
+编辑app.vue查看样式
+模板语法{{}}
+v-bind
+v-on
+v-if
+v-for
+v-model
+data方法以及里面的数据
+methods属性以及里面的方法
+v-bind绑定class的特殊处理
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- {{ message }}
+    <span :title="message">我是span标签</span>
+    <div v-for="(item, index) in arr" :key="index">
+      {{ item }}
+    </div>
+    <div v-if="true">我是if指令</div>
+    <div v-else-if="false">我是else-if指令</div>
+    <div v-else>我是else指令</div>
+    <div @click="test">点我</div>
+    <input type="text" v-model="text" /> -->
+    <!-- <input type="text" v-model.number="first" />
+    <select v-model="ope">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    <input type="text" v-model.number="second" />
+    <span>={{ result }}</span>
+    <div @click="comput">计算</div> -->
+    <div v-for="(item, index) in newsList" :key="index">
+      <span
+        :class="[
+          index == 0
+            ? 'red'
+            : index == 1
+            ? 'green'
+            : index == 2
+            ? 'orange'
+            : 'putong',
+        ]"
+        >{{ index + 1 }}</span
+      >{{ item.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+//() =>  ===  function()
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+  data: () => {
+    return {
+      newsList: [
+        { title: "第一条新闻" },
+        { title: "第二条新闻" },
+        { title: "第三条新闻" },
+        { title: "第4条新闻" },
+        { title: "第5条新闻" },
+        { title: "第6条新闻" },
+        { title: "第7条新闻" },
+        { title: "第8条新闻" },
+      ],
+      // first: "",
+      // second: "",
+      // ope: "+",
+      // result: "1",
+      // message: "hello",
+      // arr: [1, 2, 3, 4, 5],
+      // text: "123",
+    };
+  },
+  methods: {
+    test() {
+      console.log("点击了按钮");
+    },
+    comput() {
+      switch (this.ope) {
+        case "+": {
+          this.result = parseInt(this.first) + parseInt(this.second);
+          break;
+        }
+        case "-": {
+          this.result = parseInt(this.first) - parseInt(this.second);
+          break;
+        }
+        case "*": {
+          this.result = parseInt(this.first) * parseInt(this.second);
+          break;
+        }
+        case "/": {
+          this.result = parseInt(this.first) / parseInt(this.second);
+          break;
+        }
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +103,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.red {
+  color: red;
+  font-size: 30px;
+}
+.green {
+  color: green;
+  font-size: 30px;
+}
+.orange {
+  color: orange;
+  font-size: 30px;
+}
+.putong {
+  color: black;
+  font-size: 20px;
 }
 </style>
